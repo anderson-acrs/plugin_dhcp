877b6378c7ebd9a451ea957112561625fa85cf5e
anderson.claudio <anderson.claudio@pop-rn.rnp.br>
2020-09-09 20:53:34 +0000

    updated models and admin done

--- admin.py
+++ admin.py
@@ -1,9 +1,22 @@
 from django.contrib import admin
-from .models import Responsavel
+from .models import Dhcp, Ipfixo, Responsavel, Servico
 
 # Register your models here.
+
+
+@admin.register(Dhcp)
+class DhcpAdmin(admin.ModelAdmin):
+   list_display = ('prefixes', 'netmask', 'id_domain', 'gateway', 'opcao', 'tipo', 'ip_inicial', 'ip_final', 'data_criacao')
+
 
 @admin.register(Responsavel)
 class ResponsavelAdmin(admin.ModelAdmin):
     list_display = ('nome', 'contato')
 
+@admin.register(Ipfixo)
+class IpfixoAdmin(admin.ModelAdmin):
+    list_display = ('id_prefixes', 'mac', 'ip_host', 'host', 'defaultleasetime','maxleasetime' )
+    
+@admin.register(Servico)
+class ServicoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'protocol', 'port_number','ip_address', 'description')
--- models.py
+++ models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 
-class Servicos(models.Model):
-    id_servico = models.IntegerField(primary_key=True)
+class Servico(models.Model):
+    id_servico = models.AutoField(primary_key=True)
     nome = models.CharField(max_length = 20)
     protocol = models.CharField(max_length=7)
     port_number = models.IntegerField()
@@ -12,7 +12,7 @@ class Servicos(models.Model):
 
 
 class Dhcp(models.Model):
-    id_prefixes = models.IntegerField(primary_key=True)
+    id_prefixes = models.AutoField(primary_key=True)
     prefixes = models.GenericIPAddressField()
     netmask = models.GenericIPAddressField()
     id_domain = models.IntegerField()
@@ -22,11 +22,7 @@ class Dhcp(models.Model):
     ip_inicial = models.GenericIPAddressField()
     ip_final = models.GenericIPAddressField()
     data_criacao = models.DateTimeField('date published')
-    id_servico = models.ForeignKey(
-        to='dhcpd.Dhcp',
-        on_delete=models.PROTECT,
-        related_name='servico',
-    ) 
+    id_servico = models.ForeignKey(Servico, on_delete=models.CASCADE)
     id_resp = models.ForeignKey(
         to='dhcpd.Dhcp',
         on_delete=models.PROTECT,
@@ -34,7 +30,7 @@ class Dhcp(models.Model):
     )
 
 class Ipfixo(models.Model):
-    id_ipfixo = models.IntegerField(primary_key=True)
+    id_ipfixo = models.AutoField(primary_key=True)
     id_prefixes = models.ForeignKey(Dhcp, on_delete=models.CASCADE)
     mac = models.CharField(max_length=16)
     ip_host = models.GenericIPAddressField()
@@ -43,6 +39,6 @@ class Ipfixo(models.Model):
     maxleasetime = models.IntegerField()
 
 class Responsavel(models.Model):
-    id_resp = models.IntegerField(primary_key=True)
+    id_resp = models.AutoField(primary_key=True)
     nome = models.CharField(max_length=20)
     contato = models.CharField(max_length=20)
