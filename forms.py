24c2740984e7b3003e1117dc6052a069fafbcb54
anderson.claudio <anderson.claudio@pop-rn.rnp.br>
2020-09-11 22:30:05 +0000

    updated forms

---
+++ forms.py
@@ -0,0 +1,40 @@
+from django import forms
+from utilities.forms import BootstrapMixin
+from .models import Dhcp
+
+BLANK_CHOICE = (("", "---------"),)
+
+
+class DhcpForm(BootstrapMixin, forms.ModelForm):
+    class Meta:
+        model = Dhcp
+        fields = [
+            'prefixes', 
+            'netmask',
+            'id_domain',
+            'gateway',
+            'opcao',
+            'tipo',
+            'ip_inicial',
+            'ip_final',
+            'data_criacao',
+            'id_resp',            
+        ]
+
+class DhcpFilterForm(BootstrapMixin, forms.ModelForm)
+    q = forms.CharField(
+    required=False,
+    label="Buscador",
+    )
+    status = forms.CharField(
+        choices=BLANK_CHOICE,
+        required=False
+    )
+    class Meta:
+        model = Dhcp
+        fields = [
+            'q',
+            'prefixes',
+            'gateway',
+            'ip_inicial',
+        ]
