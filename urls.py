010e0f4e5f16a838ffed80d81b99594ca41c971e
anderson.claudio <anderson.claudio@pop-rn.rnp.br>
2020-09-18 22:54:57 +0000

    updating object views

--- urls.py
+++ urls.py
@@ -6,6 +6,8 @@ app_name = 'dhcpd'
 urlpatterns = [
     path('', views.DhcpView.as_view(), name='dhcp_lista'),
     path('dhcp', views.DhcpListView.as_view() , name='dhcp_list'),
+    path('add', views.DhcpCreateView.as_view(), name='dhcp_add'),
+    #path('delete', views.DhcpDeleteView.as_view(), name='dhcp_delete'),
     #path('api/', include('appview.api.urls')),
     #path('random/', views.RandomSwitchView.as_view(), name='random_switch'),
 
--- views.py
+++ views.py
@@ -5,21 +5,38 @@ from .forms import DhcpForm, DhcpFilterForm
 from .filter import DhcpFilter
 from .tables import DhcpTable
 from django.views import View
-from  utilities.views import ObjectListView
+from  utilities.views import ObjectListView, ObjectEditView, ObjectDeleteView
 
 # Create your views here.
 class DhcpView(PermissionRequiredMixin, View): 
     permission_required = 'dhcpd.dhcp_view'
     def get(self,request):
+       # d = get_object_or_404(Dhcp.objects.filter(id_prefixes=pk))
         dhcp = Dhcp.objects.all()
         return render(request, 'dhcpd/dhcp_list.html',{
             'dhcp_list': dhcp,
         })
         
 class DhcpListView (ObjectListView):
-    permission_required = 'dhcpd.view_dhcplistview'
+    permission_required = 'dhcpd.view_dhcp'
     queryset = Dhcp.objects.all()
     filterset = DhcpFilter
     filterset_form = DhcpFilterForm
     table = DhcpTable
     template_name = 'dhcpd/dhcp_list.html'
+
+class DhcpCreateView(ObjectEditView):
+    permission_required = 'dhcpd.add_dhcp'
+    model = Dhcp
+    queryset = Dhcp.objects.all()
+    model_form = DhcpForm
+    template_name = 'dhcpd/dhcp_edit.html'
+    dafault_return_url = 'dhcpd:dhcp_list'
+
+class DhcpEditView(DhcpCreateView):
+    permission_required = 'dhcpd.change_dhcp'
+
+class DhcpDeleteView(ObjectDeleteView):
+    permission_required = 'dhcpd.delete_dhcp'
+    model = Dhcp
+    default_return_url = 'dhcpd:dhcp_list'
